91caac657b279645045c4d283e115f7599168440
kwiatkowskijan <70690975+kwiatkowskijan@users.noreply.github.com>
2023-06-28 13:11:03 +0200

    finish

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const task = () => {
     const tasksContainer = document.querySelector(".tasks-container");
     const newElement = document.createElement("div");
     newElement.classList.add("task-container");
-    newElement.style.background = `hsl(${Math.random() * 360}, 90%, 70%)`;
+    newElement.style.background = `hsl(${Math.random() * 360}, 50%, 70%)`;
 
     const taskSpan = document.createElement("p");
     taskSpan.textContent = taskText;
     newElement.appendChild(taskSpan);
 
     const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
+    deleteButton.textContent = "Completed";
     deleteButton.addEventListener("click", () => {
         tasksContainer.removeChild(newElement);
     });
@@ -20,17 +20,17 @@ const task = () => {
     tasksContainer.append(newElement);
 
     taskInput.value = "";
-    
+
 }
 
 
 document.getElementById("add-task-form").addEventListener("submit", function (event) {
-    if(document.getElementById("task-input").value == '') {
+    if (document.getElementById("task-input").value == '') {
         event.preventDefault();
         console.log("Wypełnij pole");
     }
     else {
-    event.preventDefault();
-    task();
+        event.preventDefault();
+        task();
     }
 });
